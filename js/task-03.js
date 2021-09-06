e13f245091187a28dbf1f1bad15759d229763185
NazarDonets <nazardonets@gmail.com>
2021-09-06 20:46:47 +0300

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,35 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li. Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+
+const imagesContainer = document.querySelector(`#gallery`);
+
+// const imagesList = images.map(element => {
+//   const imageListElement = document.createElement(`li`);
+//   const image = document.createElement(`img`);
+//   imageListElement.classList.add(`gallery-image`)
+//   imageListElement.append(image);
+//   image.src = element.url;
+//   image.alt = element.alt;
+//   image.style.display = `flex`;
+//   image.style.width = `100%`;
+//   return imageListElement;
+// });
+
+const imagesList = images.map(element => {
+  const imageElement = element.innerHTML = `<li class="gallery-item"><img src="${element.url} alt="${element.alt}" class="gallery-image"></li>`;
+  return imageElement;
+}).join("");
+
+imagesContainer.insertAdjacentHTML(`afterbegin`, imagesList);
+
