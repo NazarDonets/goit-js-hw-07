7822259442fb743b670e03ddd530790c8330b7ff
NazarDonets <nazardonets@gmail.com>
2021-09-06 22:57:21 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputField = document.querySelector(`#validation-input`);
+
+const requiredSymbols = Number(inputField.dataset.length);
+
+inputField.addEventListener(`blur`, (e) => {
+   inputField.classList.remove(`valid`, `invalid`);
+   if (e.target.value.length === requiredSymbols) {
+      inputField.classList.add(`valid`);
+   } else {
+      inputField.classList.add(`invalid`);
+   };
+});
+
+console.log(inputField);
